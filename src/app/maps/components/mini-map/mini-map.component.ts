909f336bfb4e628736c5dd21a319c32585a23d11
DevAlbert97 <dev.carloshudm@gmail.com>
2022-09-30 10:41:04 -0700

    Pantalla propiedades

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.css']
 })
-export class MiniMapComponent implements OnInit {
+export class MiniMapComponent implements AfterViewInit {
+
+  @Input() lngLat: [number, number] = [0,0];
+  @ViewChild('map') map!: ElementRef;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: this.map.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+    });
+
+    new mapboxgl.Marker().setLngLat(this.lngLat).addTo(map);
   }
 
 }
